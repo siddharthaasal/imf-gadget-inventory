8e2f9a1ea8f631b6df4276f88dcd835114da7487
siddharthaasal <siddharthaasal@gmail.com>
2025-06-12 21:38:58 +0530

    added swagger docs

--- dist/index.js
+++ dist/index.js
@@ -9,9 +9,12 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 // import './types/express';
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const yamljs_1 = __importDefault(require("yamljs"));
 const api_1 = __importDefault(require("./api"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+const swaggerDocument = yamljs_1.default.load("./swagger.yaml");
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
@@ -19,6 +22,7 @@ app.get("/", (req, res) => {
     res.send("Server for imf-gadgets-inventory says hello");
 });
 app.use("/", api_1.default);
+app.use("/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
 const portNumber = process.env.portNumber || 3001;
 app.listen(portNumber, () => {
     console.log(`Server running on port ${portNumber}`);
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 // import './types/express';
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
 
 import mainRouter from "./api";
 
 dotenv.config();
 const app = express();
+const swaggerDocument = YAML.load("./swagger.yaml");
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
@@ -19,7 +22,7 @@ app.get("/", (req, res) => {
 })
 
 app.use("/", mainRouter);
-
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const portNumber = process.env.portNumber || 3001;
 app.listen(portNumber, () => {
